29fd95d8e4fd1a995e7c67d59b3970fb31503515
Adam <adam.siehen@gmail.com>
2025-02-23 11:15:47 +0100

    koniec - lab 4

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 # modele musimy zaimportować
-from .models import Category, Topic
+from .models import Category, Topic, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ["created_at", "modified_at"]
 
 # a następnie zarejestrować (pokazano najprostszy przypadek)
 admin.site.register(Category)
 admin.site.register(Topic)
+admin.site.register(Post, PostAdmin)
