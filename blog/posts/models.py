e4d3fd8a412673e96866d19580dd57fb987ad22c
Adam <adam.siehen@gmail.com>
2025-03-01 16:14:23 +0100

    Lab5 i Lab6

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -11,6 +11,9 @@ class Topic(models.Model):
     name = models.CharField(max_length=60)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
+ #Lab5 - przeciązanie operatorów
+    def __str__(self):
+        return self.name
     
 class Post(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
@@ -18,6 +21,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False),
     modified_at = models.DateTimeField(auto_now=True, editable=False),
     created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    
-    
+#Lab5      
+    def __str__(self):
+        return self.content[:20] + "..."
     
