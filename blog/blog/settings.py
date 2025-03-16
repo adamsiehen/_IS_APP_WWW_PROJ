9294c9017a2c8bb89ba2aa104d3a191e57c7ae42
Adam <adam.siehen@gmail.com>
2025-03-16 15:57:17 +0100

    Lab 8 - swagger po poprawkach

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'posts',
     'debug_toolbar',
     'rest_framework',
